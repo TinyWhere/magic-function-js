d96b17886ce513d77704512383b83c6c151c5828
where.liu <where.liu@huolala.cn>
2020-12-02 10:38:18 +0800

    feat: clear

--- src/function.js
+++ src/function.js
@@ -1,7 +1,6 @@
 /**
  * 为数组添加删除方法
  *
- * @format
  * @param 接收一个数组元素
  * @return boolean
  */
@@ -14,15 +13,20 @@ Array.prototype.remove = function (item) {
   }
   return false;
 };
+
 /**
- * 为window.location 添加查询params的方法
- * @param 接收一个字符串
+ * 为window.location 添加查询 params 的方法
+ *
+ * @param b 接收一个字符串
  * @return null|string
  */
 window.location.query = function (b) {
-  var reg = new RegExp("(^|&|/?)" + b + "=([^&]*)(&|$)", "i");
-  var r = window.location.href.substr(1).match(reg);
-  if (r != null) return r[2];
+  const reg = new RegExp("(^|&|/?)" + b + "=([^&]*)(&|$)", "i");
+  const r = window.location.href.substr(1).match(reg);
+  if (r != null) {
+    return r[2];
+  }
+
   return null;
 };
 /**
@@ -30,14 +34,15 @@ window.location.query = function (b) {
  * @return null|string
  */
 window.location.platform = function () {
-  var platform = null;
-  var useragent = navigator.userAgent;
+  let platform = null;
+  const userAgent = navigator.userAgent;
   const platforms = ["Windows NT", "Linux", "Macintosh", "Android", "iPhone", "iPod", "iPad", "Windows Phone"];
-  for (i = 0; i <= platforms.length; i++) {
-    if (useragent.indexOf(platforms[i]) > -1) {
+  for (let i = 0; i <= platforms.length; i++) {
+    if (userAgent.indexOf(platforms[i]) > -1) {
       platform = platforms[i];
       break;
     }
   }
+
   return platform;
 };
